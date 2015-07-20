6549ff5715f689bc72a915830b7bb19c23bc7375
Javier Virviescas <virtoja13@hotmail.com>
2015-07-20 18:31:12 -0500

    cambios

--- js/index.js
+++ js/index.js
@@ -3,6 +3,7 @@ $(document).ready(function(){
 	$('#aRegistrarUsuariosId').click(function (e) {
 		ruta = '<li><a href="#">Home</a></li><li><a href="#">Gestión de Usuarios</a></li><li><a href="#">Registrar Usuarios</a></li>';
 		$("#divInfoGeneral").hide();
+		$("#divGestionOp").hide();
 		$("#divRegistrarUsuarios").show();
 		$("#h2GestionUsuario").text(" Registrar Usuarios");
 		$("#ulRuta").html(ruta);
@@ -11,9 +12,33 @@ $(document).ready(function(){
 	$('#aConsultarUsuarios').click(function (e) {
 		ruta = '<li><a href="#">Home</a></li><li><a href="#">Gestión de Usuarios</a></li><li><a href="#">Consultar Usuarios</a></li>';
 		$("#divInfoGeneral").hide();
+		$("#divGestionOp").hide();
 		$("#divRegistrarUsuarios").show();
 		$("#h2GestionUsuario").text(" Consultar Usuarios");
 		$("#ulRuta").html(ruta);
     });
+	
+	$('#aOperadores').click(function (e) {
+		ruta = '<li><a href="#">Home</a></li><li><a href="#">Gestión de Usuarios</a></li><li><a href="#">Operadores</a></li>';
+		$("#divInfoGeneral").hide();
+		$("#divRegistrarUsuarios").hide();
+		$("#divUsuariosOp").hide();
+		$("#divGestionOp").show();
+		$("#h2GestionOperador").text(" Operadores");
+		$("#ulRuta").html(ruta);
+    });
+	
+	$('#aUsuarioXOp').click(function (e) {
+		ruta = '<li><a href="#">Home</a></li><li><a href="#">Gestión de Usuarios</a></li><li><a href="#">Usuarios por Operador</a></li>';
+		$("#divInfoGeneral").hide();
+		$("#divRegistrarUsuarios").hide();
+		$("#divUsuariosOp").show();
+		$("#divGestionOp").show();
+		$("#h2GestionOperador").text(" Usuarios por Operador");
+		$("#ulRuta").html(ruta);
+    });
+	
+	
+	
 	
 });
